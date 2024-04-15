c29c47badb388b54acd5678960450c814a153e39
guilherme chaves <gchaves.guilherme@gmail.com>
2024-04-15 14:26:02 -0300

    feat: add TransactionsTable component

---
+++ 02-dtmoney/src/components/TransactionsTable/index.tsx
@@ -0,0 +1,40 @@
+import { Container } from './styles';
+
+export function TransactionsTable() {
+    return (
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Título</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento de website</td>
+                        <td className="deposit">R$1.200,00</td>
+                        <td>Desenvolvimento</td>
+                        <td>20/02/2021</td>
+                    </tr>
+                    <tr>
+                        <td>Salário</td>
+                        <td className="deposit">R$5.200,00</td>
+                        <td>Salário</td>
+                        <td>20/02/2021</td>
+                    </tr>
+                    <tr>
+                        <td>Aluguel</td>
+                        <td className="withdraw">- R$1.100,00</td>
+                        <td>Casa</td>
+                        <td>17/02/2021</td>
+                    </tr>
+                </tbody>
+            </table>
+        </Container>
+    )
+}
+
