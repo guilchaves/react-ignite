147b248571001c55b2f0d03fd6954bf6c7f8375c
guilherme chaves <gchaves.guilherme@gmail.com>
2024-04-16 16:33:27 -0300

    reafactor: use ContextAPI to project

--- 02-dtmoney/src/components/Summary/index.tsx
+++ 02-dtmoney/src/components/Summary/index.tsx
@@ -3,7 +3,7 @@ import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import totalImg from '../../assets/total.svg'
 
-export function Summary(){
+export function Summary() {
     return (
         <Container>
             <div>
--- 02-dtmoney/src/components/TransactionsTable/index.tsx
+++ 02-dtmoney/src/components/TransactionsTable/index.tsx
@@ -1,24 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useContext } from 'react';
+import { TransactionsContext } from '../../TransactionsContext';
 import { Container } from './styles';
-import { api } from '../../services/api';
-
-
-interface Transaction {
-    id: number;
-    title: string;
-    amount: number;
-    type: string;
-    category: string;
-    createdAt: string;
-}
 
 export function TransactionsTable() {
-    const [transactions, setTransactions] = useState<Transaction[]>([]);
-
-    useEffect(() => {
-        api.get('transactions')
-            .then(res => setTransactions(res.data.transactions))
-    }, []);
+    const transactions = useContext(TransactionsContext);
 
     function convertDateTime(date: Date) {
         return new Intl.DateTimeFormat('pt-BR').format(date);
