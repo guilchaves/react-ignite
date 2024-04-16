7c8776d2a7eea6f706aa57bf0e78c66caf2f476f
guilherme chaves <gchaves.guilherme@gmail.com>
2024-04-16 17:54:39 -0300

    feat: add calculated values to summary

--- 02-dtmoney/src/components/Summary/index.tsx
+++ 02-dtmoney/src/components/Summary/index.tsx
@@ -2,8 +2,28 @@ import { Container } from "./styles";
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import totalImg from '../../assets/total.svg'
+import { TransactionsContext } from "../../TransactionsContext";
+import { useContext } from "react";
 
 export function Summary() {
+    const { transactions } = useContext(TransactionsContext)
+
+    const summary = transactions.reduce((acc, transaction) => {
+        if (transaction.type === 'deposit') {
+            acc.deposits += transaction.amount;
+            acc.total += transaction.amount;
+        } else {
+            acc.withdraws += transaction.amount
+            acc.total -= acc.withdraws;
+        }
+
+        return acc;
+    }, {
+        deposits: 0,
+        withdraws: 0,
+        total: 0
+    })
+
     return (
         <Container>
             <div>
@@ -11,21 +31,21 @@ export function Summary() {
                     <p>Entradas</p>
                     <img src={incomeImg} alt="Entradas" />
                 </header>
-                <strong>R$1000,00</strong>
+                <strong>{summary.deposits}</strong>
             </div>
             <div>
                 <header>
                     <p>Saídas</p>
                     <img src={outcomeImg} alt="Saídas" />
                 </header>
-                <strong>-R$500,00</strong>
+                <strong>-{summary.withdraws}</strong>
             </div>
             <div className="highlight-background">
                 <header>
                     <p>Total</p>
                     <img src={totalImg} alt="Total" />
                 </header>
-                <strong>R$500,00</strong>
+                <strong>{summary.total}</strong>
             </div>
         </Container>
     )
