38fe98312c98e876abc96281b165bf2651ab2887
guilherme chaves <gchaves.guilherme@gmail.com>
2024-04-25 19:02:49 -0300

    feat: save user email to faunaDB

--- 03-ignews/src/pages/api/auth/[...nextauth].ts
+++ 03-ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,14 +1,61 @@
+import { query as q } from "faunadb"
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from '../../../services/fauna'
 
 export const authOptions = {
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      scope: 'read:user'
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+      authorization: {
+        params: {
+          scope: "read:user"
+        }
+      }
     }),
   ],
+  jwt: {
+    signingKey: process.env.SIGNING_KEY,
+  },
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user.user
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              {
+                data: { email }
+              }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(email)
+              )
+            )
+          )
+        )
+
+        return true
+      } catch (error) {
+
+        return false
+      }
+    }
+  }
 }
 
 export default NextAuth(authOptions)
